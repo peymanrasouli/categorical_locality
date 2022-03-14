728f0c7c53ee517c77985f5c132cdcabfffe44e9
peymanrasouli <peyman.rasouli1@gmail.com>
2022-03-14 13:23:49 +0100

    updated

--- explanation_based_neighborhood.py
+++ explanation_based_neighborhood.py
@@ -12,7 +12,7 @@ class ExplanationBasedNeighborhood():
                 dataset,
                 N_samples):
 
-        self.X = FrequencyBasedRandomSampling(X, N_samples * 20)
+        self.X = X #FrequencyBasedRandomSampling(X, N_samples * 20)
         self.y = model.predict(self.X)
         self.model = model
         self.dataset = dataset
